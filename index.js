381bb54ce71b2e4e722482ef298aa1cbfb96b557
apushkarev <apushkarev@users.noreply.github.com>
2021-05-24 12:28:00 +0300

    added express listener

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('hello world!');
+const express = require('express');
+const app = express();
+
+app.listen(80, () => console.log('listening at 80'));
+app.use(epxress.static('public'));
