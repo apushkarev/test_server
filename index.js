6b2e0ddd4773e13f5c95713f4f6ee141cb3f0483
apushkarev <apushkarev@users.noreply.github.com>
2021-05-26 14:15:26 +0300

    update: env

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+require('dotenv').config();
 const app = express();
 
 app.listen(80, () => console.log('listening at 80'));
 // app.use(express.static('public'));
+
+console.log(process.env.PORT);
 
 app.get('/', (req, res) => {
     res.send({ message: 'Hello WWW!' });
