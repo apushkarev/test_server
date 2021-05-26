48e18b68ecc038ed477d35108ce55d7338b9491e
apushkarev <apushkarev@users.noreply.github.com>
2021-05-26 13:46:27 +0300

    update: static page on get

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ app.listen(80, () => console.log('listening at 80'));
 // app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send({ message: 'Hello WWW!' });
+    res.send(express.static('public'));
 });
