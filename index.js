e946d252275306d297dd396c38841762ad88313e
apushkarev <apushkarev@users.noreply.github.com>
2021-05-26 13:41:37 +0300

    update: commented out static page delivery

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.listen(80, () => console.log('listening at 80'));
-app.use(express.static('public'));
+// app.listen(80, () => console.log('listening at 80'));
+// app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.send({ message: 'Hello WWW!' });
