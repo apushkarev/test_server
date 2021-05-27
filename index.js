c069e0a87949438ac6bfaf261dd3e677fc7a5014
apushkarev <apushkarev@users.noreply.github.com>
2021-05-27 19:09:02 +0300

    Update: log process.argv

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.listen(80, () => console.log('listening at 80'));
 app.use(express.static('public'));
 
-console.log(process);
+console.log(process.argv);
 
 // app.get('/', (req, res) => {
 //     res.send({ message: 'Hello WWW!' });
