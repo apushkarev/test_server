64d1c6e4a1ce6dc6007dfd6002b855b3f321c003
apushkarev <apushkarev@users.noreply.github.com>
2021-06-14 15:58:03 +0300

    Update: mor logs

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 app.listen(80, () => console.log('listening at 80'));
 app.use(express.static('public'));
 
+console.log('ARGS');
 console.log(process.argv);
 
 // app.get('/', (req, res) => {
