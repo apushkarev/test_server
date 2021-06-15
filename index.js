5f574864f8d6d81827db5e56fb73a700bf4f0133
apushkarev <apushkarev@users.noreply.github.com>
2021-06-15 19:29:16 +0300

    Update: require fs

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const fs = require('fs')
 
 app.listen(80, () => console.log('listening at 80'));
 app.use(express.static('public'));
 
-// console.log(process.argv);
+console.log(fs);
 
 app.get('/', (req, res) => {
     res.send({ message: 'Hello WWW!' });
