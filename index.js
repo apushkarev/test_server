8a530cf65ebff510be43526e021b4f25fe9ec875
apushkarev <apushkarev@users.noreply.github.com>
2021-06-15 19:49:37 +0300

    Update: post call

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const fs = require('fs')
 
-app.listen(80, () => console.log('listening at 80'));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.send({ message: 'Hello WWW!' });
 });
+
+app.post('/api/saveFile', (req, res) => {
+  conslole.log(req);
+  conslole.log({...req.body});
+  res.status(202).send({message: 'Accepted'})
+});
+
+app.listen(80, () => console.log('listening at 80'));
