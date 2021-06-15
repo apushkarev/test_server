a24d1773e60f180900e54e3a3178c8b7d14949dd
apushkarev <apushkarev@users.noreply.github.com>
2021-06-15 19:31:18 +0300

    Update: remove fs logging

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const fs = require('fs')
 app.listen(80, () => console.log('listening at 80'));
 app.use(express.static('public'));
 
-console.log(fs);
-
 app.get('/', (req, res) => {
     res.send({ message: 'Hello WWW!' });
 });
