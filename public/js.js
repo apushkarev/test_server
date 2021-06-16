f9e1a7a46046afb60a2a0ae8d5ae93bd0a236c40
apushkarev <apushkarev@users.noreply.github.com>
2021-06-16 17:37:33 +0300

    Fix: paths

--- public/js.js
+++ public/js.js
@@ -1,8 +1,14 @@
 async function sendTestRequest() {
+
+  const headers = {};
+  headers['Content-Type'] = 'application/json';
+
+  const data = JSON.stringify('test');
+
   const response = await fetch(`https://avt-editor.lenflash.com/api/saveFile`, {
     method: 'POST',
-    {'Content-Type': 'application/json'},
-    {'Test'}
+    headers,
+    data
   });
 
   console.log(await response.json());
